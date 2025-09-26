6a137bd4d85f45abb02ea5ccaac1d7e483e26f32
Tabish_Ilahi_Ansari <119218722+tabish3210@users.noreply.github.com>
2025-09-26 09:12:11 +0530

    Create Problem no. 611. Valid Triangle Number.cpp

---
+++ Problem no. 611. Valid Triangle Number.cpp
@@ -0,0 +1,33 @@
+PROBLEM   NUMBER                                     :                                                 611. Valid Triangle Number
+
+
+// Friday, 26 September 2025
+
+
+//  Two Pointers  |  Binary Search |  Greedy
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return 0;
+        sort(nums.begin(), nums.end());
+        int count = 0;
+        for (int k = n - 1; k >= 2; --k) {
+            int i = 0, j = k - 1;
+            while (i < j) {
+                if (nums[i] + nums[j] > nums[k]) {
+                    count += (j - i);
+                    --j;
+                } else {
+                    ++i;
+                }
+            }
+        }
+        return count;
+    }
+};
