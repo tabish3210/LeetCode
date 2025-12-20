0a747a4442d794c474cffd96bcdb8b3cb03dff59
Tabish_Ilahi_Ansari <119218722+tabish3210@users.noreply.github.com>
2025-12-20 09:09:51 +0530

    Create Problem no. 944. Delete Columns to Make Sorted.cpp

---
+++ Problem no. 944. Delete Columns to Make Sorted.cpp
@@ -0,0 +1,24 @@
+PROBLEM    NUMBER                      :                           944. Delete Columns to Make Sorted
+
+
+// Saturday, 20 December 2025
+
+
+// Matrix Traversing
+
+class Solution {
+    bool isUnsorted(const vector<string>& strs, int j) {
+        for (int i = 1; i < strs.size(); i++)
+            if (strs[i][j] < strs[i - 1][j])
+                return true;
+        return false;
+    }
+
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int res = 0;
+        for (int j = 0; j < strs[0].size(); j++)
+            res += isUnsorted(strs, j);
+        return res;
+    }
+};
